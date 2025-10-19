4bd460ab101e60fbbcc151b18b505eabd3a472f4
mbmoncalvillo <mb.moncalvillo@irakasle.eus>
2025-10-19 18:30:54 +0200

    primera version

---
+++ src/principal/HolaMundo.java
@@ -0,0 +1,15 @@
+package principal;
+
+import java.util.Scanner;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {		 
+
+		Scanner teclado=new Scanner(System.in);
+		System.out.println("Introduce una letra");
+		teclado.nextLine();
+		teclado.close();
+	}
+
+}
